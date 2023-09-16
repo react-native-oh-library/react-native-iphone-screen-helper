341b1bd56cf08f4ac67b08734a1ada1b1c592e39
chlee1001 <chlee1001@naver.com>
2023-09-16 20:23:08 +0900

    🔧 refactor(index.js): use device id to determine iPhone model for better accuracy ➕ feat(index.js): add legacy functions for backward compatibility
    The previous method of determining the iPhone model based on screen dimensions was not always accurate. Now, the device id is used to look up the model in a predefined list, which provides more reliable results. The legacy functions have been preserved and marked as such to maintain backward compatibility for projects that may still rely on the old behavior.

--- index.d.ts
+++ index.d.ts
@@ -4,3 +4,12 @@ export function ifIphoneX<T, U>(iphoneXVal: T, regularVal: U): T | U;
 export function ifIphoneX<T>(iphoneXVal: T): T;
 export function getStatusBarHeight(safe?: boolean): number;
 export function getBottomSpace(): number;
+
+
+export function isIphoneXLegacy(): boolean;
+export function isDynamicIslandLegacy(): boolean;
+export function ifIphoneXLegacy<T, U>(iphoneXVal: T, regularVal: U): T | U;
+export function ifIphoneXLegacy<T>(iphoneXVal: T): T;
+export function getStatusBarHeightLegacy(safe?: boolean): number;
+export function getBottomSpaceLegacy(): number;
+
