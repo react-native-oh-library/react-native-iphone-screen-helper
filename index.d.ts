91f21f7536e751bed5045290372e7345787a665c
chlee1001 <chlee1001@naver.com>
2023-09-16 20:32:23 +0900

    🔨 refactor(index.d.ts): remove optional 'safe' parameter from getStatusBarHeight and getStatusBarHeightLegacy functions The 'safe' parameter was removed from the getStatusBarHeight and getStatusBarHeightLegacy functions to simplify their usage. This change was made because the 'safe' parameter was not being used in most cases, and its removal does not affect the functionality of these functions.

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ export function isIphoneX(): boolean;
 export function isDynamicIsland(): boolean;
 export function ifIphoneX<T, U>(iphoneXVal: T, regularVal: U): T | U;
 export function ifIphoneX<T>(iphoneXVal: T): T;
-export function getStatusBarHeight(safe?: boolean): number;
+export function getStatusBarHeight(): number;
 export function getBottomSpace(): number;
 
 
@@ -10,6 +10,6 @@ export function isIphoneXLegacy(): boolean;
 export function isDynamicIslandLegacy(): boolean;
 export function ifIphoneXLegacy<T, U>(iphoneXVal: T, regularVal: U): T | U;
 export function ifIphoneXLegacy<T>(iphoneXVal: T): T;
-export function getStatusBarHeightLegacy(safe?: boolean): number;
+export function getStatusBarHeightLegacy(): number;
 export function getBottomSpaceLegacy(): number;
 
